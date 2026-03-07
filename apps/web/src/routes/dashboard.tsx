62b7facd8383f035288451883288e44ddd736a8c
riz1 <rizwan.k.off@gmail.com>
2026-03-07 13:49:36 +0530

    cleanup

--- apps/web/src/routes/dashboard.tsx
+++ apps/web/src/routes/dashboard.tsx
@@ -3,27 +3,27 @@ import { createFileRoute, redirect } from "@tanstack/react-router";
 import { getUser } from "@/functions/get-user";
 
 export const Route = createFileRoute("/dashboard")({
-  component: RouteComponent,
-  beforeLoad: async () => {
-    const session = await getUser();
-    return { session };
-  },
-  loader: async ({ context }) => {
-    if (!context.session) {
-      throw redirect({
-        to: "/login",
-      });
-    }
-  },
+	component: RouteComponent,
+	beforeLoad: async () => {
+		const session = await getUser();
+		return { session };
+	},
+	loader: async ({ context }) => {
+		if (!context.session) {
+			throw redirect({
+				to: "/login",
+			});
+		}
+	},
 });
 
 function RouteComponent() {
-  const { session } = Route.useRouteContext();
+	const { session } = Route.useRouteContext();
 
-  return (
-    <div>
-      <h1>Dashboard</h1>
-      <p>Welcome {session?.user.name}</p>
-    </div>
-  );
+	return (
+		<div>
+			<h1>Dashboard</h1>
+			<p>Welcome {session?.user.name}</p>
+		</div>
+	);
 }
